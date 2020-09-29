887bf7e268ab0a8f1508497691ff63b7e6dbdd34
wengyou <13637937511@163.com>
2020-09-29 21:26:19 +0800

    09.29

--- 算法/recursion.js
+++ 算法/recursion.js
@@ -54,3 +54,63 @@ var buildTree = function(preorder, inorder) {
 };
 
 buildTree([3,9,20,15,7],[9,3,15,20,7])
+
+/**
+ * 题目：实现函数double Power(double base, int exponent)，求base的exponent次方。
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+
+//错误示例
+var myPow1 = function(x, n) {
+    let pow = 1;
+    if(n==0) {
+        return 1
+    } else if (n>0){
+        for(let i = 0; i < n; i++) {
+            pow = pow * x;
+        }
+    } else {
+        pow = 1/myPow(x,-n)
+    }
+    console.log(pow)
+    return pow;
+};
+
+//内置函数
+var myPow2 = function(x, n) {
+    return Math.pow(x, n)
+}
+
+/**
+ * 题目：用字符串数组作为井字游戏的游戏板 board。
+ * 当且仅当在井字游戏过程中，玩家有可能将字符放置成游戏板所显示的状态时，才返回 true。
+ * @param {string[]} board
+ * @return {boolean}
+ */
+var validTicTacToe = function(board) {
+    let Xcount = 0, Ocount = 0;
+    board.map(item => {
+        for(let i = 0; i < 3; i++) {
+            item[i] === 'X' && Xcount++;
+            item[i] === 'O' && Ocount++;
+        }
+    })
+    if(Xcount !== Ocount && Xcount !== Ocount +1) return false;
+    if(win(board, 'X') && Xcount !== Ocount + 1) return false;
+    if(win(board, 'O') && Xcount !== Ocount) return false;
+    return true;
+};
+function win(board, P) {
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] === P && board[i][1] === P && board[i][2] === P) {
+            return true;
+        }
+        if (board[0][i] ===P && board[1][i] === P && board[2][i] === P) return true; 
+    }
+    if (board[0][0] === P && board[1][1] === P && board[2][2] === P) return true;
+    if (board[0][2] === P && board[1][1] === P && board[2][0] === P) return true;
+    return false;
+}
+validTicTacToe(["XXX","   ","OOO"])
