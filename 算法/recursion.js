bf8e188e9a3fdb5d40fe43d0325b81f469677f00
wengyou <13637937511@163.com>
2020-10-05 22:12:47 +0800

    10.05.22:12

--- 算法/recursion.js
+++ 算法/recursion.js
@@ -115,3 +115,30 @@ function win(board, P) {
 }
 validTicTacToe(["XXX","   ","OOO"])
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+/**
+ * 题目：给定二叉搜索树的根结点 root，返回 L 和 R（含）之间的所有结点的值的和。二叉搜索树保证具有唯一的值。
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var rangeSumBST = function(root, L, R) {
+    if (root == null) {
+        return 0
+    }
+    if (root.val < L) {
+        return rangeSumBST(root.right, L, R)
+    } else if (R < +root.val) {
+        return rangeSumBST(root.left, L, R)
+    } else {
+        return root.val + rangeSumBST(root.right, L, R) + rangeSumBST(root.left, L, R)
+    }
+};
