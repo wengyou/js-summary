104e34c1481c67c25f77d55b7e4a05c03ece5e45
wengyou <13637937511@163.com>
2020-10-09 22:35:06 +0800

    10.09.22:34

--- 算法/string.js
+++ 算法/string.js
@@ -249,3 +249,35 @@ var compress = function(chars) {
   }
   return i;
 };
+
+/**
+ * 题目：给定一个字符串来代表一个学生的出勤记录，这个记录仅包含以下三个字符：
+ * 'A' : Absent，缺勤
+ * 'L' : Late，迟到
+ * 'P' : Present，到场
+ * 如果一个学生的出勤记录中不超过一个'A'(缺勤)并且不超过两个连续的'L'(迟到),那么这个学生会被奖赏。
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+    let countA = 0, countL = 0;
+    for (let i = 0; i < s.length; i++) {
+        if(s[i] == 'A') {
+          if(countA <= 1) {
+            countA++
+          } else {
+            return false;
+          }
+        }
+        if(s[i] == 'L' && s[i+1] == 'L' && s[i+2] == 'L') {
+          return false
+        }
+    }
+    if(countA <= 1) {
+      return true
+    } else {
+      return false
+    }
+};
+
+checkRecord("PPALLL")
