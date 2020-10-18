c0542f283d02c7472c090030b8538015ec157df4
wengyou <13637937511@163.com>
2020-10-18 23:59:05 +0800

    2020.10.18.23:59

--- 算法/binary.js
+++ 算法/binary.js
@@ -17,3 +17,45 @@ var missingNumber = function(nums) {
   return left
 };
 missingNumber([0,1])
+
+/**
+ * 题目：把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ * 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+ * 例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+ * @param {number[]} numbers
+ * @return {number}
+ */
+var minArray1 = function(numbers) {
+  numbers.sort((a, b) => {
+      return a - b;
+  });
+  return numbers[0];
+};
+
+var minArray2 = function(numbers) {
+  for(let i = 0; i < numbers.length; i++) {
+      if (numbers[i] < numbers[0]) return numbers [i];
+  };
+  return numbers[0];
+};
+
+var minArray3 = function(numbers) {
+  let left = 0;
+  let right = numbers.length - 1;
+  while(left < right) {
+      //无符号位移
+      const mid = left + right >>> 1;
+      if (numbers[mid] > numbers[right]) {
+          left = mid + 1;
+      } else if (numbers[mid] < numbers[right]) {
+          right = mid
+      } else {
+          right--
+      }
+  }
+  return numbers[left]
+};
+
+var minArray4 = function(numbers) {
+  return Math.min(...numbers)
+};
